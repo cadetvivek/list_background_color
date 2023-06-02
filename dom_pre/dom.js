5943e370a84cbc87faa1aeac2a0f6ecc0cb129e3
vivek <106858243+cadetvivek@users.noreply.github.com>
2023-06-02 18:43:42 +0530

    Add files via upload
    help of dom change the list background color

---
+++ dom_pre/dom.js
@@ -0,0 +1,32 @@
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// //document.title=123
+// console.log(document.head)
+// console.log(document.doctype)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10])
+// console.log(document.forms[0])
+// console.log(document.links)
+// console.log(document.images)
+
+// GETELEMENTBYID
+
+// var headTitle = document.getElementById('head-title')
+// var header = document.getElementById('main-header')
+
+// header.style.borderBottom='solid 5px #000'
+// var heading = document.querySelector('.title');
+// heading.style.color='green'
+var items = document.getElementsByClassName('list-group-item');
+console.log(items[2])
+items[2].style.fontWeight='bold'
+items[1].style.fontWeight='bold'
+items[1].style.color='blue'
+items[2].style.backgroundColor='green'
+
+// if we want to give all background color so we need to for loop
+//  for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='yellow'
+//  }
